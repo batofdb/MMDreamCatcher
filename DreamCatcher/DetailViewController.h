9e69d397cdbc67c6d92264ba2a2e629d854f3651
Francis Bato <batofdb@gmail.com>
2015-09-25 12:43:51 -0700

    initial commit

---
+++ DreamCatcher/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  DreamCatcher
+//
+//  Created by Francis Bato on 9/14/15.
+//  Copyright (c) 2015 Francis Bato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property NSString *titleString;
+@property NSString *descriptionString;
+
+
+@end
